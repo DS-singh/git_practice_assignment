dae1e0d02c8303bae29d8f2451f7f834c234f4c0
DS-singh <deepshreesingh9@gmail.com>
2022-12-08 22:18:15 +0530

    created 2 file

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+let str= "racecar";
+  let bag=" ";
+  for (let i=str.length-1;i>=0;i--){
+    bag+=str[i];
+   }
+   if(str==bag){
+   console.log("palindrome");
+   }
+   else{
+   console.log("Not palindrome");
+   }

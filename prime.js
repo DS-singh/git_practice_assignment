5425f0a8e6591a0727232c6e69e30fc8e4981215
Yogeshsorde <dineshsorde1997@gmail.com>
2022-12-08 22:33:35 +0530

    small mistakes solved

--- prime.js
+++ prime.js
@@ -1,11 +1,18 @@
 let num= 11;
+
  let count=0;
+
  for(let i=1 ; i<=num ; i++){
+
  if((num%i)==0){
+
   count++;
+
   }
+
  }
- if( count>=1){
+ if( count==2){
+
  console.log("Prime number");
  }
 else {
